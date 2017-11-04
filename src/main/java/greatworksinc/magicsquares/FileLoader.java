03f4cb4f5d55dec9f52451f3ebc2f172d9f0936d
Simon Greene <simongreene02@gmail.com>
2017-11-04 10:58:27 -0500

    Completed inital code for CSV to List converter.

---
+++ src/main/java/greatworksinc/magicsquares/FileLoader.java
@@ -0,0 +1,36 @@
+package greatworksinc.magicsquares;
+
+import com.google.common.base.Splitter;
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.Lists;
+import com.google.common.io.Resources;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class FileLoader {
+    private static final Logger log = LoggerFactory.getLogger(FileLoader.class);
+    private static final Splitter COMMA_SPLITTER = Splitter.on(',');
+    public static List<String> fromCSVtoList(String resourceName) {
+        URL url = Resources.getResource(resourceName);
+        try {
+            List<String> rowsList = Files.readAllLines(Paths.get(url.toURI()));
+            List<String> outputList = Lists.newArrayList();
+            for (String row : rowsList) {
+                outputList.addAll(COMMA_SPLITTER.splitToList(row));
+            }
+            return outputList;
+        } catch (IOException e) {
+            log.error("The file input or output may not have been working.", e);
+        } catch (URISyntaxException e) {
+            log.error("File name syntax may have been wrong.", e);
+        }
+        return ImmutableList.of();
+    }
+}
